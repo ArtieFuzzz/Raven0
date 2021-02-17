1577ac3983cea3fb9bc73b2db680793cda7c7da0
Λｒｔｉｅ　（姻依為） <jardenz.den@gmail.com>
2021-02-17 16:35:39 +1100

    UwU update

---
+++ src/commands/randnsfw.js
@@ -0,0 +1,34 @@
+const { Command } = require('discord-akairo');
+const { KSoftClient } = require('@ksoft/api');
+const { MessageEmbed } = require('discord.js')
+
+const ksoft = new KSoftClient(process.env.KSOFT_TOKEN);
+
+class HentaiCommand extends Command {
+    constructor() {
+        super('helplesshentai', {
+           aliases: ['helplesshentai'] 
+        });
+    }
+
+    async exec(message) {
+    if (!message.guild) return true;
+    if (!message.channel.nsfw) {
+            message.util.send(':x: This command only runs in NSFW channels');
+            return true;
+        }
+
+        const { url, post } = await ksoft.images.nsfw();
+        const embed = new DiscordJS.MessageEmbed()
+        .setTitle(post.title)
+        .setFooter(`Powered by api.ksoft.si ${post.author} | Upvotes: ${post.upvotes} | Downvotes ${post.downvotes}`)
+        .setURL(post.link)
+        .setColor("RANDOM")
+        .setTimestamp()
+        .setImage(url);
+        msg.channel.send(embed);
+    message.channel.send(embed)
+    }
+}
+
+module.exports = HentaiCommand;
