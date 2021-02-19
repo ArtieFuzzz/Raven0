3cd2e282078190178edcf80335475ca858fe6378
Λｒｔｉｅ　（姻依為） <jardenz.den@gmail.com>
2021-02-19 12:56:15 +1100

    Re adding this if I can

---
+++ src/Extensions/message.js
@@ -0,0 +1,15 @@
+const { Structures } = require('discord.js');
+
+    module.exports = Structures.extend("Message", Message => class extends Message {
+        async suppessEmbed(hide) {
+            this.client.api.channels[this.channel.id].messages[this.id]['suppress-embeds'].post({
+                headers: { 
+                    "Content-Type": "raw"
+                },
+                data: {
+                    suppress: hide
+                }
+            });
+        }
+    });
+© 2021 GitHub, Inc.
