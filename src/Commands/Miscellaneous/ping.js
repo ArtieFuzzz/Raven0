1a32fd15a7603768a0bd2d53fe850219f256faf1
Λｒｔｉｅ　（姻依為） <jardenz.den@gmail.com>
2021-02-20 12:55:57 +1100

    Better ping command

--- src/Commands/Miscellaneous/ping.js
+++ src/Commands/Miscellaneous/ping.js
@@ -13,8 +13,14 @@ class PingCommand extends Command {
         });
     }
 
-    exec(message) {
-        return message.reply('Pong!');
+    async exec(message) {
+        const sent = await message.util.reply('Pong!');
+        const timeDiff = (sent.editedAt || sent.createdAt) - (message.editedAt || message.createdAt);
+        return message.util.reply([
+            'Pong!',
+            `🔂 **RTT**: ${timeDiff} ms`,
+            `💟 **Heartbeat**: ${Math.round(this.client.ws.ping)} ms`
+        ]);
     }
 }
 
