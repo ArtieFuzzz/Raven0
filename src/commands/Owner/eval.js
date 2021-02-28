de1155a39b49a72ff6aacfa29873e3c7d2535b12
Λｒｔｉｅ　（姻依為） <jardenz.den@gmail.com>
2021-02-28 18:54:58 +1100

    Stopwatch for Eval command owo

--- src/commands/Owner/eval.js
+++ src/commands/Owner/eval.js
@@ -1,6 +1,7 @@
 const { Command } = require('discord-akairo');
 const { MessageEmbed } = require('discord.js');
 const { inspect } = require('util')
+const { Stopwatch } = require('@sapphire/stopwatch')
 const prefix = process.env.PREFIX;
 
 class EvalCommand extends Command {
@@ -16,7 +17,7 @@ class EvalCommand extends Command {
                 }
         });
     }
-
+//⏱
     exec(message) {
     const embed = new MessageEmbed()
     .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true, format: 'png', size: 4096 }))
@@ -24,24 +25,30 @@ class EvalCommand extends Command {
 const args = message.content.slice(prefix.length).trim().split(/ +/g); args.shift()
 const query = args.join(' ')
 const code = (lang, code) => (`\`\`\`${lang}\n${String(code).slice(0, 1000) + (code.length >= 1000 ? '...' : '')}\n\`\`\``).replace(this.client.token, "Uh oh! I can't do that!")
+const stopwatch = new Stopwatch();
 
 if (!query) message.channel.send('Please, write something so I can evaluate!')
 else {
   try {
     const evald = eval(query)
+    const time = stopwatch.toString();
     const res = typeof evald === 'string' ? evald : inspect(evald, { depth: 0 })
 
     embed.addField('Result', code('js', res))
 
+
+    stopwatch.stop();
     if (!Boolean(res) || (!Boolean(evald) && evald !== 0)) embed.setColor('RED')
     else {
       embed
         .addField('Type', code('css', typeof evald))
+        .addField('Time ⏱', time)
         .setColor('GREEN')
     }
   } catch (error) {
     embed
       .addField('Error', code('js', error))
+      .addField('Time ⏱', stopwatch.stop().toString())
       .setColor('RED')
   } finally {
       message.channel.send(embed).catch(error => {
