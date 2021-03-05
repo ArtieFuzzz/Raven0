5068ab586ec99aea668d047dc4c1fe79afef86e5
Λｒｔｉｅ　（姻依為） <jardenz.den@gmail.com>
2021-03-05 20:53:10 +1100

    Update urban.js
    Hopefully it works actually

---
+++ src/Commands/Miscellaneous/urban.js
@@ -0,0 +1,42 @@
+const { Command } = require('discord-akairo');
+const c = require('centra');
+const { MessageEmbed } = require('discord.js');
+
+class UrbanCommand extends Command {
+	constructor() {
+		super('urban', {
+			aliases: ['urban', 'ud'],
+			category: 'Miscellaneous',
+			args: [
+				{
+					id: 'word',
+					type: 'string',
+					match: 'content',
+				}],
+			description: {
+				usage: 'urban [Word to search]',
+				examples: ['urban Femboy', 'ud simp'],
+				description: 'Search a word up on Urban Dictionary.',
+			},
+		});
+	}
+
+	async exec(message, args) {
+		if (!args.word) return message.channel.send('I\'ve got nothing to search!');
+		try {
+			const res = await c(`https://api.urbandictionary.com/v0/define?term=${args.word}`).send();
+			const { list } = await res.json();
+			const embed = new MessageEmbed()
+				.setTitle(list[0].word)
+				.setDescription(list[0].definition)
+				.setURL(list[0].permalink)
+				.setFooter(`Author: ${list[0].author} | ID: ${list[0].defid} | Upvotes: ${list[0].thumbsup} | Downvotes: ${list[0].thumbsdown}`);
+			message.channel.send(embed);
+		}
+		catch (err) {
+			return message.channel.send(err.message);
+		}
+	}
+}
+
+module.exports = UrbanCommand;
