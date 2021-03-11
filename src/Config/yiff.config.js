dc057262b1b7a0a529132cedc920d8de26be835e
Λｒｔｉｅ　（姻依為） <jardenz.den@gmail.com>
2021-03-11 19:06:30 +1100

    Update yiff.config.js

--- src/Config/yiff.config.js
+++ src/Config/yiff.config.js
@@ -1,5 +1,7 @@
+/* eslint-disable object-shorthand */
+const { version } = require('../../package.json');
 module.exports = {
 	creator: 'ArtieFuzzz#8298',
 	name: 'Sparrow',
-	version: '3.2.0'
+	version: version
 };
