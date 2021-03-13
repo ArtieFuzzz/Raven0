1ce173a227493d4cba195897c68fce65d63383f1
Λｒｔｉｅ　（姻依為） <jardenz.den@gmail.com>
2021-03-13 12:56:26 +1100

    Update snowflake.js
    Deconstruction

--- src/Commands/Owner/snowflake.js
+++ src/Commands/Owner/snowflake.js
@@ -12,27 +12,50 @@ class SnowFlakeCommand extends Command {
 			description: {
 				usage: 'snowflake',
 				examples: ['snowflake', 'genflake', 'gensnowflake'],
-				description: 'Generates a snowflake (Not a discord snowflake).',
+				description: 'Generates a discord snowflake (Not a real discord snowflake but can deconstruct one).',
 			},
 			ownerOnly: true,
+			args: [
+				{
+					id: 'snowflake',
+					type: 'string',
+					match: 'content',
+				} ],
 		});
 	}
 
-	async exec(message) {
-		const snowflake = DiscordSnowflake.generate();
-		const flake = DiscordSnowflake.deconstruct(snowflake);
+	async exec(message, args) {
+		if (!args.snowflake) {
+			const snowflake = DiscordSnowflake.generate();
+			const flake = DiscordSnowflake.deconstruct(snowflake);
 
-		const embed = new MessageEmbed()
-			.setTitle('SnowFlake Generated!')
-			.addFields(
-				{ name: 'SnowFlake', value: flake.id },
-				{ name: 'Timestamp', value: flake.timestamp },
-				{ name: 'WorkerID', value: flake.workerID },
-				{ name: 'ProcessID', value: flake.processID },
-				{ name: 'Epoch', value: flake.epoch },
-			)
-			.setFooter('Use this to convert your Timestamp: https://www.epochconverter.com/');
-		message.channel.send(embed);
+			const embed = new MessageEmbed()
+				.setTitle('SnowFlake Generated!')
+				.addFields(
+					{ name: 'SnowFlake', value: flake.id },
+					{ name: 'Timestamp', value: flake.timestamp },
+					{ name: 'WorkerID', value: flake.workerID },
+					{ name: 'ProcessID', value: flake.processID },
+					{ name: 'Epoch', value: flake.epoch },
+				)
+				.setFooter('Use this to convert your Timestamp: https://www.epochconverter.com/');
+			return message.channel.send(embed);
+		}
+		else if (args.snowflake) {
+			const flake = DiscordSnowflake.deconstruct(args.snowflake);
+
+			const embed = new MessageEmbed()
+				.setTitle('SnowFlake Deconstructed!')
+				.addFields(
+					{ name: 'SnowFlake', value: flake.id },
+					{ name: 'Timestamp', value: flake.timestamp },
+					{ name: 'WorkerID', value: flake.workerID },
+					{ name: 'ProcessID', value: flake.processID },
+					{ name: 'Epoch', value: flake.epoch },
+				)
+				.setFooter('Use this to convert your Timestamp: https://www.epochconverter.com/');
+			message.channel.send(embed);
+		}
 	}
 
 }
