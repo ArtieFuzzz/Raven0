3092545464a8cfa1c3b4c9a1b8416d287a918a01
ArtieFuzzz <jardenz.den@gmail.com>
2021-03-15 20:53:56 +1100

    3.5.2
    logs command

---
+++ src/Commands/Owner/logs.js
@@ -0,0 +1,30 @@
+const { Command } = require('discord-akairo');
+const fs = require('fs');
+
+class LogsCommand extends Command {
+
+	constructor() {
+		super('logs', {
+			aliases: ['logs'],
+			category: 'Owner',
+			ownerOnly: true,
+			description: {
+				usage: 'logs',
+				examples: ['gogs'],
+				description: 'Bot returns logs.',
+			},
+		});
+	}
+
+	async exec(message) {
+		fs.readFile('.env', (err, data) => {
+			if (err) console.log(err);
+			if (!data) return message.channel.send('No data was in the logs');
+
+			message.author.send(data.toString());
+		});
+	}
+
+}
+
+module.exports = LogsCommand;
