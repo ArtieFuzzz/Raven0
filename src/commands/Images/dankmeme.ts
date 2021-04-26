19dbde57914e02e59c5071629361a8c8d37e842d
ArtieFuzzz <jardenz.den@gmail.com>
2021-04-26 21:09:36 +1000

    dankmeme.ts

--- src/commands/Images/dankmeme.ts
+++ src/commands/Images/dankmeme.ts
@@ -4,21 +4,21 @@ import { MessageEmbed } from '../../structures/MessageEmbed'
 
 export default class AwwCommand extends Command {
   public constructor () {
-    super('dankmeme', {
-      aliases: ['dankmeme', 'dkm'],
+    super('aww', {
+      aliases: ['aww'],
       category: 'Images',
       description: 'Returns a cute command',
       ratelimit: 3
     })
 
     this.help = {
-      usage: 'dankmeme',
-      examples: ['dankmeme', 'dkm']
+      usage: 'aww',
+      examples: ['aww']
     }
   }
 
   public async exec (message: Message): Promise<Message> {
-    const { url, post } = await this.client.ksoft.images.reddit('dankmemes', { removeNSFW: true, span: 'day' })
+    const { url, post } = await this.client.ksoft.images.aww()
     const embed = new MessageEmbed()
       .setTitle(post.title)
       .setFooter(`Powered by api.ksoft.si ${post.author} | Upvotes: ${post.upvotes} | Downvotes ${post.downvotes}`)
