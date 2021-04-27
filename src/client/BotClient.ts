e6d1b86870097203b53d1eedc2f97cedf2cf4a4f
ArtieFuzzz <jardenz.den@gmail.com>
2021-04-27 22:30:59 +1000

    What is this bullshit

--- src/client/BotClient.ts
+++ src/client/BotClient.ts
@@ -103,7 +103,7 @@ export default class BotClient extends AkairoClient {
     // this.eventEmitter.on('changeStatus', async () => await this.changeStatus())
 
     // Set a startup notice. This will be overridden upon ready.
-    this.user.setActivity({ name: 'Spotify', type: 'LISTENING' })
+    this.user.setActivity({ name: `Spotify | ${process.env.CLIENT_PREFIX}`, type: 'LISTENING' })
 
     // Automate status changes and upload stat uploads.
     this.setInterval(() => this.eventEmitter.emit('changeStatus'), 5 * 60 * 1000) // every five minutes
