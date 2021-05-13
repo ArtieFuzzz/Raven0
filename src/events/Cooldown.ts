9c5180ef7ee78cc8fa13e83a873a7137d01c7440
ArtieFuzzz <jardenz.den@gmail.com>
2021-05-13 12:26:03 +1000

    Don't need to use embeds

--- src/events/Cooldown.ts
+++ src/events/Cooldown.ts
@@ -1,8 +1,8 @@
 import { Listener, Command } from 'discord-akairo'
 import { Message } from 'discord.js'
-import { MessageEmbed } from '../structures/MessageEmbed'
+// import { MessageEmbed } from '../../structures/MessageEmbed'
 
-class CooldownListener extends Listener {
+export default class CooldownListener extends Listener {
   constructor () {
     super('cooldown', {
       emitter: 'commandHandler',
@@ -10,13 +10,12 @@ class CooldownListener extends Listener {
     })
   }
 
-  public async exec (message: Message, command: Command, remaining: any): Promise<Message> {
-    const embed = new MessageEmbed()
+  public async exec (message: Message, command: Command, remaining: number): Promise<Message> {
+    /* const embed = new MessageEmbed()
       .setColor('RED')
       // eslint-disable-next-line @typescript-eslint/restrict-template-expressions, @typescript-eslint/no-base-to-string
       .setDescription(`⚙️ You are on a cooldown for the command: \`${command}\`. Please wait: \`${remaining.toFixed(2) / 1000}s\``)
-    return await message.channel.send({ embed })
+    return await message.channel.send({ embed }) */
+    return await message.util.reply(`Slow down! Wait \`${remaining}\``)
   }
 }
-
-module.exports = CooldownListener
