2cfd8c7a76b33c4db48a09768b41476e5b474b9a
ArtieFuzzz <jardenz.den@gmail.com>
2021-05-18 16:50:50 +1000

    Noice

--- src/events/Cooldown.ts
+++ src/events/Cooldown.ts
@@ -10,12 +10,12 @@ export default class CooldownListener extends Listener {
     })
   }
 
-  public async exec (message: Message, command: Command, remaining: number): Promise<Message> {
+  public async exec (message: Message, command: Command, remaining: any): Promise<Message> {
     /* const embed = new MessageEmbed()
       .setColor('RED')
       // eslint-disable-next-line @typescript-eslint/restrict-template-expressions, @typescript-eslint/no-base-to-string
       .setDescription(`⚙️ You are on a cooldown for the command: \`${command}\`. Please wait: \`${remaining.toFixed(2) / 1000}s\``)
     return await message.channel.send({ embed }) */
-    return await message.util.reply(`Slow down! Wait \`${remaining}\``)
+    return await message.util.reply(`Slow down! Wait \`${remaining.toFixed(2) / 1000}s\`s`)
   }
 }
