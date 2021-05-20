782f781b8ce8f022a2159856d331821b8158dd7b
ArtieFuzzz <jardenz.den@gmail.com>
2021-05-20 21:10:29 +1000

    Merge SentryClient.ts with index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
 import BotClient from './client/BotClient'
-import Sentry from './lib/SentryClient'
 import dotenv from 'dotenv'
+import Sentry from '@sentry/node'
+import os from 'os'
+import conf from './config'
+import { version as discordVersion } from 'discord.js'
+import { version as akairoVersion } from 'discord-akairo'
+
 dotenv.config()
 
 const client = new BotClient()
 
-Sentry()
+Sentry.init({
+	dsn: conf.sentryURI,
+	release: `raven0@${conf.version}`,
+	tracesSampleRate: 1.0
+})
+
+Sentry.configureScope(scope => {
+	scope.setTags({
+		host: `${os.hostname()}`,
+		'akairo-version': akairoVersion,
+		'discord.js-version': discordVersion
+	})
+})
 
 client.start()
