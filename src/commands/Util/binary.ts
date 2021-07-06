9546e674b4a5bece2ccbf249c8651ef6f103330d
♥ ArtieFuzzz ♥ <jardenz.den@gmail.com>
2021-07-06 13:28:37 +1000

    Better eval and binary command

--- src/commands/Util/binary.ts
+++ src/commands/Util/binary.ts
@@ -12,41 +12,48 @@ export default class BinaryCommand extends Command {
 			cooldown: 5000,
 			args: [
 				{
-					id: 'op',
-					type: 'string',
-					match: 'separate'
-				},
-				{
 					id: 'str',
 					type: 'string',
-					match: 'content'
+					match: 'rest'
+				},
+				{
+					id: 'encodeFlag',
+					match: 'flag',
+					type: 'flag',
+					flag: ['-e', '--encode']
+				},
+				{
+					id: 'decodeFlag',
+					match: 'flag',
+					type: 'flag',
+					flag: ['-d', '--decode']
 				}
 			]
 		})
 
 		this.help = {
-			usage: 'binary <encode | decode> <text>',
-			examples: ['binary encode computers are cool :)']
+			usage: 'binary <-e --encode | -d --decode> <text>',
+			examples: ['binary -e computers are cool :)']
 		}
 	}
 
-	public async exec (message: Message, { op, str }: { op: string, str: string}): Promise<Message> {
-		if (op[0].toLowerCase() === 'encode') return await this.Encode(message, { op, str })
-		if (op[0].toLowerCase() === 'decode') return await this.Decode(message, { op, str })
+	public async exec (message: Message, { str, encodeFlag, decodeFlag }: { str: string, encodeFlag: boolean, decodeFlag: boolean }): Promise<Message> {
+		if (encodeFlag) return await this.Encode(message, str)
+		if (decodeFlag) return await this.Decode(message, str)
 		else {
-			return await message.reply('Options: Encode | Decode')
+			return await message.reply('Options / Flags: -e --encode | -d --decode')
 		}
 	}
 
-	private async Encode (message: Message, { op, str }: { op: string, str: string}) {
-		const res = await c(`https://some-random-api.ml/binary?text=${str.slice(op[0].length).trim()}`).send()
+	private async Encode (message: Message, str: string) {
+		const res = await c(`https://some-random-api.ml/binary?text=${str}`).send()
 		const { binary } = res.json
 
 		return await message.channel.send(binary)
 	}
 
-	private async Decode (message: Message, { op, str }: { op: string, str: string}) {
-		const res = await c(`https://some-random-api.ml/binary?decode=${str.slice(op[0].length).trim()}`).send()
+	private async Decode (message: Message, str: string) {
+		const res = await c(`https://some-random-api.ml/binary?decode=${str}`).send()
 		const { text } = res.json
 
 		return await message.channel.send(text)
