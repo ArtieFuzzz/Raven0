7af79dd22621aa4e3c709079a2904cf16bc7ee45
♥ ArtieFuzzz ♥ <jardenz.den@gmail.com>
2021-07-06 15:51:54 +1000

    Update Cooldown.ts
    Just in-case it's a serve case of a whole server using the same or multiple commands

--- src/events/Cooldown.ts
+++ src/events/Cooldown.ts
@@ -20,7 +20,7 @@ export default class CooldownListener extends Listener {
       .setDescription(`⚙️ You are on a cooldown for the command: \`${command}\`. Please wait: \`${remaining.toFixed(2) / 1000}s\``)
     return await message.channel.send({ embed }) */
 		// eslint-disable-next-line @typescript-eslint/restrict-template-expressions, @typescript-eslint/no-base-to-string
-		this.logger.warn(`${message.author.tag} Exceeded Ratelimit / Cooldown`, `Command: ${command}`)
+		this.logger.warn(`${message.author.tag} Exceeded Ratelimit / Cooldown`, `Command: ${command}`, `Server ${message.guild.name} (${message.guild.id})`)
 		return await message.util.reply(`Slow down! Wait \`${remaining.toFixed(2) / 1000}\`s`)
 	}
 }
